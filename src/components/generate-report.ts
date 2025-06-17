6e83161addbe1f6e9db42000be92637a2830aa3a
Thuan vo <voxuanthuan280194@gmail.com>
2025-06-17 09:26:51 +0700

    feat: update get last logtime instead hard code for yesterday or friday

--- src/components/generate-report.ts
+++ src/components/generate-report.ts
@@ -1,4 +1,4 @@
-import { getDateInfo } from './date-utils';
+import { getSmartDateLabel } from './date-utils';
 import { getAutoClipboardConfig } from './config-utils';
 import { fetchAllTasks, fetchUserDisplayName, fetchPreviousWorkdayTasks } from './task-fetcher';
 import { buildMainReport, buildTodoList, combineReport } from './report-builder';
@@ -7,11 +7,13 @@ import TempoFetcher from './tempo/fetcher';
 import TempoFormatter from './tempo/formatter';
 
 export async function generateDailyReport() {
-    const dateInfo = getDateInfo();
     const autoClipboard = getAutoClipboardConfig();
     const {open, inProgress} = await fetchAllTasks();
     const user = await fetchUserDisplayName();
-    const yesterdayTasks = await fetchPreviousWorkdayTasks(user.accountId);
+    const previousWorkdayResult = await fetchPreviousWorkdayTasks(user.accountId);
+    
+    // Use the smart date label based on the actual date found
+    const smartDateLabel = getSmartDateLabel(previousWorkdayResult.actualDate);
 
     const fetcher = new TempoFetcher(user.accountId);
     const formatter = new TempoFormatter();
@@ -19,7 +21,7 @@ export async function generateDailyReport() {
     const  Worklogs = await fetcher.fetchLastSixDaysWorklogs();
     
     // const totalHours = calculateTotalHours(tasks.yesterdayTasks, dateInfo.previousDay);
-    const mainReport = buildMainReport(dateInfo.previousDayLabel, inProgress, yesterdayTasks);
+    const mainReport = buildMainReport(smartDateLabel, inProgress, previousWorkdayResult.tasks);
     const todoList = buildTodoList(open);
     // const timeLog = buildTotalHoursNote(totalHours);
     const workLogContent = formatter.formatWorkLogContent(Worklogs);
